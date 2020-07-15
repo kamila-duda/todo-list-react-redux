a32b43b4dc28e65591699bd36bf66c0b41754479
Kamila Duda <kakus86@o2.pl>
2020-07-15 09:30:00 +0200

    Updates

---
+++ precache-manifest.6ea4b70a69d8e99cf86731c65b227f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5791de0c52287f3c65055f8e6d03d68",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "a4b7a2b0a6e6c313015d",
+    "url": "/todo-list-react/static/css/main.4eeb08f6.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b7a2b0a6e6c313015d",
+    "url": "/todo-list-react/static/js/main.0f9076b3.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  }
+]);
